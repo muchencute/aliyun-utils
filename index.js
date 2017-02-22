dfae2e6af260f1ed4ad9e3344b103d715a18acdd
nothingmi <nothingmi@muchencute.com>
2017-02-22 17:29:11 +0800

    完成签名

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const URL = require('url');
+const crypto = require('crypto');
+
 class AliyunUtils {
 
     /**
@@ -7,7 +10,15 @@ class AliyunUtils {
      * @returns {string} 签名结果
      */
     static signature(url, appSecret) {
-        return 'empty';
+        let query = URL.parse(url).query;
+        let params = query.split('&');
+        for (let param of params) {
+            let components = param.split('=');
+            param = `${encodeURIComponent(components[0])}=${encodeURIComponent(components[1])}`;
+        }
+        params.sort();
+        let stringToSign = `GET&%2F&${encodeURIComponent(params.join('&'))}`;
+        return crypto.createHmac('sha1', appSecret).update(stringToSign).digest('base64');
     }
 
 }
